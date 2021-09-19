fabea8b8438a595b27f837df0c7a6244dc924833
Inna <inna.kachinskaya13@gmail.com>
2021-09-19 14:57:32 +0300

    Add element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const element = React.createElement(
+//   "h1",
+//   { classNames: "heading-title" },
+//   "Hello, React!"
+// );
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+//аналогично на JSX
+const name = "React";
+
+const elem = (
+  <h1 className="heading-title">
+    <span>
+      <a href="https://ru.reactjs.org/">Hello, {name}!</a>
+    </span>
+  </h1>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(elem, document.getElementById("root"));
