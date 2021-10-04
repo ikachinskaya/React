f96391b2b2222a2f4f301bc369043a0327632883
Inna <inna.kachinskaya13@gmail.com>
2021-10-04 21:40:34 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,54 +1,96 @@
 import { Component } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Aloha from "./components/Aloha";
+import Clicker from "./components/Clicker";
+import SignInForm from "./components/SignInForm";
+import StopWatch from "./components/StopWatch";
 import "./index.css";
+
 class App extends Component {
   render() {
     return (
-      <>
-        <button onClick={() => console.log(window.innerHeight)}>
-          innerHeight
-        </button>
-        <button onClick={() => console.log(window.innerWidth)}>
-          innerWidth
-        </button>
-        <button onClick={() => window.open("https://ru.wikipedia.org")}>
-          Open wiki
-        </button>
-        <button onClick={() => window.close()}>Х</button>
-        <button onClick={() => console.log(window.screen)}>Screen</button>
-        <button onClick={() => console.log(window.location.port)}>
-          Location
-        </button>
-        <button
-          onClick={() => window.location.assign("https://ru.wikipedia.org")}
-        >
-          Assign
-        </button>
-        <button
-          onClick={() => window.location.replace("https://ru.wikipedia.org")}
-        >
-          Replace
-        </button>
-        <button onClick={() => window.location.reload()}>Reload</button>
-        <button onClick={() => console.log(window.location.href)}>URL</button>
-        <button onClick={() => console.log(window.location.toString())}>
-          URL
-        </button>
-        <button onClick={() => console.log(window.navigator)}>Navigator</button>
-        <button
-          onClick={() =>
-            window.navigator.geolocation.getCurrentPosition((pos) =>
-              console.log(pos)
-            )
-          }
-        >
-          GeoLocation
-        </button>
-        <button onClick={() => console.log(window.navigator.userAgent)}>UserAgent</button>
-      </>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="link">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts" className="link">
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+        </Switch>
+        <h1>My Projects</h1>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/components/Aloha" className="link">
+                Aloha
+              </Link>
+            </li>
+            <li>
+              <Link to="/components/Clicker" className="link">
+                Clicker
+              </Link>
+            </li>
+            <li>
+              <Link to="/components/SignInForm" className="link">
+                SignInForm
+              </Link>
+            </li>
+            <li>
+              <Link to="/components/StopWatch" className="link">
+                StopWatch
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/components/Aloha">
+            <Aloha />
+          </Route>
+          <Route path="/components/Clicker">
+            <Clicker />
+          </Route>
+          <Route path="/components/SignInForm">
+            <SignInForm />
+          </Route>
+          <Route path="/components/StopWatch">
+            <StopWatch />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Contacts = () => <div>Contacts</div>;
+
 export default App;
 
-//BOM: alert, confirm, prompt, setTimeOut, setInterval
+//exact -сравнивает путь с точностью
