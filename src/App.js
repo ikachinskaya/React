68f134b907846006518853150241538d5d831c85
Inna <inna.kachinskaya13@gmail.com>
2021-10-04 12:47:45 +0300

    BOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import { Component } from "react";
-import Userloader from "./components/UserLoader/userLoader";
-
+import "./index.css";
 class App extends Component {
   render() {
-    return <Userloader />;
+    return (
+      <>
+        <button onClick={() => console.log(window.innerHeight)}>
+          innerHeight
+        </button>
+        <button onClick={() => console.log(window.innerWidth)}>
+          innerWidth
+        </button>
+        <button onClick={() => window.open("https://ru.wikipedia.org")}>
+          Open wiki
+        </button>
+        <button onClick={() => window.close()}>Х</button>
+        <button onClick={() => console.log(window.screen)}>Screen</button>
+        <button onClick={() => console.log(window.location.port)}>
+          Location
+        </button>
+        <button
+          onClick={() => window.location.assign("https://ru.wikipedia.org")}
+        >
+          Assign
+        </button>
+        <button
+          onClick={() => window.location.replace("https://ru.wikipedia.org")}
+        >
+          Replace
+        </button>
+        <button onClick={() => window.location.reload()}>Reload</button>
+        <button onClick={() => console.log(window.location.href)}>URL</button>
+        <button onClick={() => console.log(window.location.toString())}>
+          URL
+        </button>
+        <button onClick={() => console.log(window.navigator)}>Navigator</button>
+        <button
+          onClick={() =>
+            window.navigator.geolocation.getCurrentPosition((pos) =>
+              console.log(pos)
+            )
+          }
+        >
+          GeoLocation
+        </button>
+        <button onClick={() => console.log(window.navigator.userAgent)}>UserAgent</button>
+      </>
+    );
   }
 }
 
 export default App;
+
+//BOM: alert, confirm, prompt, setTimeOut, setInterval
