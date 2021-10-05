5f8cbe5dbfcd5f4b9fc7e9ce868537444bfef4e1
Inna <inna.kachinskaya13@gmail.com>
2021-10-05 16:53:03 +0300

    Add render props. DataProvider.

---
+++ src/components/DataProvider/dataProvider.jsx
@@ -0,0 +1,52 @@
+/*
+1. Прогрузка данных: что грузить или как грузить
+2. Отправить данные куда-то, где они отрисуются
+3. За отрисовку этих данных он не отвечает
+*/
+
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+class DataProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  load() {
+    this.setState({
+      isLoading: true,
+    });
+    const { loadData } = this.props;
+    loadData()
+      .then((data) => {
+        this.setState({ data, isLoading: false });
+      })
+      .catch((error) => {
+        this.setState({
+          error,
+          isLoading: false,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.load();
+  }
+
+  render() {
+    const { render } = this.props;
+    const layout = render(this.state);
+    return layout;
+  }
+}
+
+DataProvider.propTypes = {
+  loadData: PropTypes.func.isRequired,
+  render: PropTypes.func.isRequired,
+};
+export default DataProvider;
--- src/pages/LoaderPage/loaderPage.jsx
+++ src/pages/LoaderPage/loaderPage.jsx
@@ -1,11 +1,66 @@
 import React, { Component } from "react";
-import Looserloader from "../../components/LooserLoader/looserLoader.jsx";
+import DataProvider from "../../components/DataProvider/dataProvider";
 
 class LoaderPage extends Component {
+  getUsers = () => {
+    return fetch("/users.json").then((res) => res.json());
+  };
+
+  renderUsers = (state) => {
+    if (state.error) {
+      return <h1>{state.error.message}</h1>;
+    }
+    return (
+      <ol>
+        {state.data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>
+    );
+  };
+
+  getNotebooks = () => {
+    return fetch("/notebooks.json").then((res) => res.json());
+  };
+
+  renderNotebooks = (state) => {
+    if (state.error) {
+      return <h1>{state.error.message}</h1>;
+    }
+    return (
+      <ul>
+        {state.data.map((notebook) => (
+          <li key={notebook.id}>{notebook.name}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  getPicture = () => {
+    return new Promise((res, rej) => {
+      res(
+        "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_28/1587661/dogs-age-years-kb-inline-200707.jpg"
+      );
+    });
+  };
+
+  renderPicture = (state) => {
+    return (
+      <div style={{ width: "400px" }}>
+        <img src={state.data} alt="image" style={{ width: "100%" }} />
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
-        <Looserloader />
+        <DataProvider
+          loadData={this.getNotebooks}
+          render={this.renderNotebooks}
+        />
+        <DataProvider loadData={this.getUsers} render={this.renderUsers} />
+        <DataProvider loadData={this.getPicture} render={this.renderPicture} />
       </div>
     );
   }
