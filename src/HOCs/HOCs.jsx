e419f664a0f7485327262e35e3f075017c54470a
Inna <inna.kachinskaya13@gmail.com>
2021-10-05 23:47:06 +0300

    ADD HOCs

---
+++ src/HOCs/HOCs.jsx
@@ -0,0 +1,25 @@
+import { ThemeContext, UserContext } from "../contexts";
+
+export const withTheme = (Component) => {
+  return function ComponentWithTheme(props) {
+    return (
+      <ThemeContext.Consumer>
+        {([theme, setTheme]) => {
+          return <Component theme={theme} setTheme={setTheme} {...props} />;
+        }}
+      </ThemeContext.Consumer>
+    );
+  };
+};
+
+export const withUser = (Component) => {
+  return function CompWithUser(props) {
+    return (
+      <UserContext.Consumer>
+        {(user) => {
+          return <Component user={user} {...props} />;
+        }}
+      </UserContext.Consumer>
+    );
+  };
+};
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,42 +1,59 @@
 import React from "react";
 import cx from "classnames";
-import { UserContext, ThemeContext } from "./../../contexts/index.js";
 import styles from "./header.module.scss";
 import CONSTANS from "../../constans.js";
+import { withTheme, withUser } from "../../HOCs/HOCs.jsx";
 const { THEMES } = CONSTANS;
 
 const Header = (props) => {
-  const { theme, setTheme } = props;
+  const { theme, setTheme, user } = props;
+
+  const headerClasses = cx(styles.container, {
+    [styles.darkTheme]: theme === THEMES.DARK,
+    [styles.lightTheme]: theme === THEMES.LIGHT,
+  });
+
+  const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
 
   return (
-    <UserContext.Consumer>
-      {(user) => {
-        const headerClasses = cx(styles.container, {
-          [styles.darkTheme]: theme === THEMES.DARK,
-          [styles.lightTheme]: theme === THEMES.LIGHT,
-        });
-        const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-        return (
-          <header className={headerClasses}>
-            <h1>Hello, {user.fullName}</h1>
-            <button className={styles.btn} onClick={() => setTheme(otherTheme)}>
-              Switch theme
-            </button>
-          </header>
-        );
-      }}
-    </UserContext.Consumer>
+    <header className={headerClasses}>
+      <h1>Hello, {user.fullName}</h1>
+      <button className={styles.btn} onClick={() => setTheme(otherTheme)}>
+        Switch theme
+      </button>
+    </header>
   );
 };
 
-const HeaderWithTheme = (props) => {
-  return (
-    <ThemeContext.Consumer>
-      {([theme, setTheme]) => {
-        return <Header theme={theme} setTheme={setTheme} />;
-      }}
-    </ThemeContext.Consumer>
-  );
-};
+// const HeaderWithTheme = (props) => {
+//   return (
+//     <ThemeContext.Consumer>
+//       {([theme, setTheme]) => {
+//         return <Header theme={theme} setTheme={setTheme} />;
+//       }}
+//     </ThemeContext.Consumer>
+//   );
+// };
 
-export default HeaderWithTheme;
+//=====================================================
+
+//=====================================================
+//не полностью универсально
+// const withContext = (Component, Context) => {
+//   return function ComponentWithContext(props) {
+//     return (
+//       <Context.Consumer>
+//         {([theme, setTheme]) => {
+//           return <Component theme={theme} setTheme={setTheme} />;
+//         }}
+//       </Context.Consumer>
+//     );
+//   };
+// };
+// const HeaderWithTheme = withContext(Header, ThemeContext);
+
+const HeaderWithTheme = withTheme(Header);
+
+const HeaderWithThemeAndUser = withUser(HeaderWithTheme);
+
+export default HeaderWithThemeAndUser;
