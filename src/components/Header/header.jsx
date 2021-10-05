deee359f56d796648ca093aa334e429592e9bb86
Inna <inna.kachinskaya13@gmail.com>
2021-10-05 22:08:29 +0300

    Add Header, HeaderWithTheme

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,34 +5,38 @@ import styles from "./header.module.scss";
 import CONSTANS from "../../constans.js";
 const { THEMES } = CONSTANS;
 
-const Header = () => {
-  const renderThemeContext = ([theme, setTheme]) => {
-    const headerClasses = cx(styles.container, {
-      [styles.darkTheme]: theme === THEMES.DARK,
-      [styles.lightTheme]: theme === THEMES.LIGHT,
-    });
+const Header = (props) => {
+  const { theme, setTheme } = props;
 
-    return (
-      <UserContext.Consumer>
-        {(user) => {
-          const otherTheme =
-            theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-          return (
-            <header className={headerClasses}>
-              <h1>Hello, {user.fullName}</h1>
-              <button
-                className={styles.btn}
-                onClick={() => setTheme(otherTheme)}
-              >
-                Switch theme
-              </button>
-            </header>
-          );
-        }}
-      </UserContext.Consumer>
-    );
-  };
-  return <ThemeContext.Consumer>{renderThemeContext}</ThemeContext.Consumer>;
+  return (
+    <UserContext.Consumer>
+      {(user) => {
+        const headerClasses = cx(styles.container, {
+          [styles.darkTheme]: theme === THEMES.DARK,
+          [styles.lightTheme]: theme === THEMES.LIGHT,
+        });
+        const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+        return (
+          <header className={headerClasses}>
+            <h1>Hello, {user.fullName}</h1>
+            <button className={styles.btn} onClick={() => setTheme(otherTheme)}>
+              Switch theme
+            </button>
+          </header>
+        );
+      }}
+    </UserContext.Consumer>
+  );
 };
 
-export default Header;
+const HeaderWithTheme = (props) => {
+  return (
+    <ThemeContext.Consumer>
+      {([theme, setTheme]) => {
+        return <Header theme={theme} setTheme={setTheme} />;
+      }}
+    </ThemeContext.Consumer>
+  );
+};
+
+export default HeaderWithTheme;
