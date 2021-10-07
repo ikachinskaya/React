9e9b5ba3187271d1f89a711114be58000292e799
Inna <inna.kachinskaya13@gmail.com>
2021-10-08 01:15:57 +0300

    Add useCallback

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useCallback, useEffect } from "react";
 import cx from "classnames";
 import styles from "./header.module.scss";
 import { ThemeContext, UserContext } from "../../contexts/index.js";
@@ -8,12 +8,41 @@ const { THEMES } = CONSTANS;
 const Header = (props) => {
   const [theme, setTheme] = useContext(ThemeContext);
   const { fullName, userImg } = useContext(UserContext);
+  const [value, setValue] = useState(1);
+
   const headerClasses = cx(styles.container, {
     [styles.darkTheme]: theme === THEMES.DARK,
     [styles.lightTheme]: theme === THEMES.LIGHT,
   });
 
   const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+  //=========================================================
+
+  //const logValue = () => console.log(value);
+
+  //полезно, когда нужно передать колбэк потомкам
+  //сохраняет колбэк, который меняется только тогда, когда меняются значения одной из зависимостей
+  const logValue = useCallback(() => {
+    console.log(value);
+  }, [value]);
+
+  //отловить, когда LogValue меняется, отрабатывается при ререндере
+  //зависит от logValue
+  //побочные эффекты
+  useEffect(() => {
+    console.log("LOG создался");
+  }, [logValue]);
+
+  const changeValue = ({ target: { value } }) => setValue(Number(value));
+  //=========================================================
+  const sayHello = useCallback(() => {
+    alert(`Hello, ${fullName}`);
+  }, [fullName]);
+
+  useEffect(() => {
+    console.log("HELLO создался");
+  }, [sayHello]);
+  //=========================================================
 
   return (
     <header className={headerClasses}>
@@ -22,6 +51,10 @@ const Header = (props) => {
       <button className={styles.btn} onClick={() => setTheme(otherTheme)}>
         Switch theme
       </button>
+      <button onClick={logValue}>LOG value</button>
+      <input type="number" onChange={changeValue} />
+
+      <button onClick={sayHello}>HELLO</button>
     </header>
   );
 };
