4a7e91fbf755907e5455fe916870bf931beaf9c0
Inna <inna.kachinskaya13@gmail.com>
2021-10-07 23:14:20 +0300

    Add reexport of contexts

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,11 +1,2 @@
-import { createContext } from "react";
-export const UserContext = createContext();
-export const ThemeContext = createContext();
-
-//можно делать так, но есть ли смысл..
-// export const UserContext = createContext({
-//   id: 1,
-//   fullName: "Test Testovich",
-//   userImg:
-//     "https://toppng.com//public/uploads/preview/vu-thi-ha-user-pro-icon-115534024853ae3gswzwd.png",
-// });
+export {default as ThemeContext} from './themeContext.js'
+export {default as UserContext} from './userContext.js'
