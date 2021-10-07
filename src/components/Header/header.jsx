fbe1447ef73029539cb6edf16cfb29f6fa7ec970
Inna <inna.kachinskaya13@gmail.com>
2021-10-08 01:55:17 +0300

    Add useMemo

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,22 @@
-import React, { useState, useContext, useCallback, useEffect } from "react";
+import React, { useState, useContext, useMemo } from "react";
 import cx from "classnames";
 import styles from "./header.module.scss";
 import { ThemeContext, UserContext } from "../../contexts/index.js";
 import CONSTANS from "../../constans.js";
 const { THEMES } = CONSTANS;
 
+function computeValue(number) {
+  let i = 0;
+  for (let j = 0; j < 1_000_000_000; j++) {
+    i++;
+  }
+  return number ** 2;
+}
+
 const Header = (props) => {
   const [theme, setTheme] = useContext(ThemeContext);
   const { fullName, userImg } = useContext(UserContext);
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState(15);
 
   const headerClasses = cx(styles.container, {
     [styles.darkTheme]: theme === THEMES.DARK,
@@ -18,63 +26,28 @@ const Header = (props) => {
   const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
   //=========================================================
 
-  //const logValue = () => console.log(value);
+  const changeValue = ({ target: { value } }) => setValue(Number(value));
 
-  //полезно, когда нужно передать колбэк потомкам
-  //сохраняет колбэк, который меняется только тогда, когда меняются значения одной из зависимостей
-  const logValue = useCallback(() => {
-    console.log(value);
+  const changeTheme = (e) => setTheme(otherTheme);
+
+  //const newValue = computeValue(value);
+
+  //useMemo так же, как useCallback,
+  //только useCallback для функций
+  //useMemo для любых значений
+  const newValue = useMemo(() => {
+    return computeValue(value);
   }, [value]);
-
-  //отловить, когда LogValue меняется, отрабатывается при ререндере
-  //зависит от logValue
-  //побочные эффекты
-  useEffect(() => {
-    console.log("LOG создался");
-  }, [logValue]);
-
-  //зависит он target: { value } , т.е. снаружи не от чего не зависит
-  //зависит от того, что прийдет из него
-  //в массив завсимостей ничего писать не нужно
-  //пишем только то, что пришло снаружи
-  const changeValue = useCallback(
-    ({ target: { value } }) => setValue(Number(value)),
-    []
-  );
-
-  useEffect(() => {
-    console.log("changeValue создался");
-  }, [changeValue]);
-
-  //=========================================================
-  const sayHello = useCallback(() => {
-    alert(`Hello, ${fullName}`);
-  }, [fullName]);
-
-  useEffect(() => {
-    console.log("HELLO создался");
-  }, [sayHello]);
-  //=========================================================
-
-  //setTheme в зависимости не нужно писать
-  const changeTheme = useCallback(() => setTheme(otherTheme), [otherTheme]);
-
-  useEffect(() => {
-    console.log("changeTheme создался");
-  }, [changeTheme]);
-
   //=========================================================
   return (
     <header className={headerClasses}>
-      <h1>Hello, {fullName}</h1>
+      <h1>{newValue}</h1>
       <img src={userImg} alt={fullName} />
       <button className={styles.btn} onClick={changeTheme}>
         Switch theme
       </button>
-      <button onClick={logValue}>LOG value</button>
-      <input type="number" onChange={changeValue} />
 
-      <button onClick={sayHello}>HELLO</button>
+      <input type="number" onChange={changeValue} />
     </header>
   );
 };
